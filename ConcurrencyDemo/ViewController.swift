30c66aaff72be86bd3d1ff15f30959052fdcbb60
Kalyanraju Malla <kalyanraju.m@tcs.com>
2016-02-16 22:41:25 +0530

    Heavy Loading Resolved - Serial Queue
    Loading Images with out freezing main thread by using one of the GCD
    Technique - Serial Queue

---
+++ ConcurrencyDemo/ViewController.swift
@@ -0,0 +1,106 @@
+//
+//  ViewController.swift
+//  ConcurrencyDemo
+//
+//  Created by Hossam Ghareeb on 11/15/15.
+//  Copyright © 2015 Hossam Ghareeb. All rights reserved.
+//
+
+import UIKit
+
+let imageURLs = ["http://www.planetware.com/photos-large/F/france-paris-eiffel-tower.jpg", "http://adriatic-lines.com/wp-content/uploads/2015/04/canal-of-Venice.jpg", "http://algoos.com/wp-content/uploads/2015/08/ireland-02.jpg", "http://bdo.se/wp-content/uploads/2014/01/Stockholm1.jpg"]
+
+class Downloader {
+    
+    class func downloadImageWithURL(url:String) -> UIImage!
+    {
+        
+        let data = NSData(contentsOfURL: NSURL(string: url)!)
+        print("data",data);
+        return UIImage(data: data!)
+    }
+}
+
+class ViewController: UIViewController
+{
+
+    @IBOutlet weak var imageView1: UIImageView!
+    
+    @IBOutlet weak var imageView2: UIImageView!
+    
+    @IBOutlet weak var imageView3: UIImageView!
+    
+    @IBOutlet weak var imageView4: UIImageView!
+    
+    @IBOutlet weak var sliderValueLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func didClickOnStart(sender: AnyObject)
+    {
+        
+   
+
+        let serialQueue = dispatch_queue_create("com.ppam.imageQueue", DISPATCH_QUEUE_SERIAL)
+        
+//        let serialQueue = dispatch_queue_create("com.ppam.imageQueue", DISPATCH_QUEUE_CONCURRENT)
+
+        
+        dispatch_async(serialQueue) { () -> Void in
+                
+            let img1 = Downloader.downloadImageWithURL(imageURLs[0])
+  
+            dispatch_async(dispatch_get_main_queue(),{
+                self.imageView1.image = img1
+            })
+        }
+        
+        dispatch_async(serialQueue) { () -> Void in
+            
+            let img2 = Downloader.downloadImageWithURL(imageURLs[1])
+            
+            dispatch_async(dispatch_get_main_queue(), {
+                
+                self.imageView2.image = img2
+            })
+            
+        }
+        
+        
+        dispatch_async(serialQueue) { () -> Void in
+            
+            let img3 = Downloader.downloadImageWithURL(imageURLs[2])
+            
+
+            
+            dispatch_async(dispatch_get_main_queue(), {
+                
+                self.imageView3.image = img3
+            })
+            
+        }
+        dispatch_async(serialQueue) { () -> Void in
+            
+            let img4 = Downloader.downloadImageWithURL(imageURLs[3])
+            
+            dispatch_async(dispatch_get_main_queue(), {
+                
+                self.imageView4.image = img4
+            })
+        }
+    }
+    @IBAction func sliderValueChanged(sender: UISlider)
+    {
+        self.sliderValueLabel.text = "\(sender.value * 100.0)"
+    }
+
+}
+
